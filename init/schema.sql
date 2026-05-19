beba17403fd377e4b0c7e97a0efc6f9cde3307c2
Paolo BERTINI <bertin_p@etna-alternance.net>
2026-05-19 02:14:45 +0200

    correction db

--- init/schema.sql
+++ init/schema.sql
@@ -1,15 +1,34 @@
--- CREATE TABLE users (
---     id SERIAL PRIMARY KEY NOT NULL,
---     user_identifier VARCHAR(100) NOT NULL UNIQUE,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
---     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
--- );
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
 
-CREATE TABLE discussions (
-    id SERIAL PRIMARY KEY NOT NULL,
-    -- user_id INTEGER REFERENCES users(id) NOT NULL,
-    caractere TEXT,
-    discussion TEXT,
+CREATE TABLE characters (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    system_prompt TEXT,
+    description_ia TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE conversations (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    user_id UUID REFERENCES users(id) NOT NULL,
+    character_id UUID REFERENCES characters(id) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE messages (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    conversation_id UUID REFERENCES conversations(id) NOT NULL,
+    role_ia ENUM('user', 'assistant') NOT NULL,
+    content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
